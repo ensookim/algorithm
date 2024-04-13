0ed1e0c0a33411094ab3409f88f6fec3ae4e8f85
ensookim <7.8204620u@gmail.com>
2024-04-13 12:43:41 +0900

    04.13

---
+++ BaekJoon/1181.py
@@ -0,0 +1,14 @@
+n = int(input())
+lst = []
+
+for i in range(n):
+    lst.append(input())
+lst = list(set(lst))
+lst.sort()	
+lst.sort(key = len)	
+
+for i in lst:
+    print(i)
+
+# print(type(lst))
+# print(type(n))
