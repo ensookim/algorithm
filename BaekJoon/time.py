8f7e36d5e1e83b2389352bb4b014dc3f116a1e28
Soo <7.8204620u@gmail.com>
2024-10-28 19:49:49 +0900

    10.28

---
+++ BaekJoon/time.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+count =0
+
+for i in range(n+1):
+    for m  in range(60):
+        for s in range(60):
+            
+            if "3" in str(i)+ str(m) + str(s):
+                count += 1
+                
+
+print(count)
